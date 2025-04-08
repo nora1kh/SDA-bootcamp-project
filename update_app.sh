9572f90d3abf1448228cc669542cd894a2cdf08b
Ubuntu <azureuser@MyVMproject.qsielqogjuuejmis5pwymi0ref.bx.internal.cloudapp.net>
2025-04-08 12:30:37 +0000

    Your commit message here

--- update_app.sh
+++ update_app.sh
@@ -4,20 +4,21 @@ set -e
 date
 echo "Updating Python application on VM..."
 
-APP_DIR="/home/azureuser/SDA-bootcamp-project"
-GIT_REPO="https://${GITHUB_TOKEN}@github.com/nora1kh/SDA-bootcamp-project.git"
+APP_DIR="/home/azureuser/my-python-app"
+GIT_REPO="https://github.com/<your-github-username>/my-python-app.git"
 BRANCH="main"
 
 # Update code
 if [ -d "$APP_DIR" ]; then
     sudo -u azureuser bash -c "cd $APP_DIR && git pull origin $BRANCH"
 else
-    sudo -u azureuser git clone -b $BRANCH "$GIT_REPO" "$APP_DIR"
+    sudo -u azureuser git clone -b $BRANCH "https://$GITHUB_TOKEN@$GIT_REPO" "$APP_DIR"
+    sudo -u azureuser bash -c "cd $APP_DIR"
 fi
 
- Install dependencies
-sudo -u azureuser /home/azureuser/SDA-bootcamp-project/venv/bin/pip install --upgrade pip
-sudo -u azureuser /home/azureuser/SDA-bootcamp-project/venv/bin/pip install -r "$APP_DIR/requirements.txt"
+# Install dependencies
+sudo -u azureuser /home/azureuser/my-python-app/venv/bin/pip install --upgrade pip
+sudo -u azureuser /home/azureuser/my-python-app/venv/bin/pip install -r "$APP_DIR/requirements.txt"
 
 # Restart the service
 sudo systemctl restart myapp
