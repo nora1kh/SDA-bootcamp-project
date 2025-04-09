ec4c8b27705567821205e1897dd97a386b01b7c8
Ubuntu <azureuser@MyVMproject.qsielqogjuuejmis5pwymi0ref.bx.internal.cloudapp.net>
2025-04-09 08:07:12 +0000

    Major updates to app — updated scripts and configurations

--- update_app.sh
+++ update_app.sh
@@ -4,21 +4,23 @@ set -e
 date
 echo "Updating Python application on VM..."
 
-APP_DIR="/home/azureuser/my-python-app"
-GIT_REPO="https://github.com/<your-github-username>/my-python-app.git"
+APP_DIR="/home/azureuser/SDA-bootcamp-project"
+GIT_REPO="https://github.com/nora1kh/SDA-bootcamp-project.git"
 BRANCH="main"
 
 # Update code
 if [ -d "$APP_DIR" ]; then
-    sudo -u azureuser bash -c "cd $APP_DIR && git pull origin $BRANCH"
+    # sudo -u azureuser bash -c "cd $APP_DIR && git pull origin $BRANCH"
+    sudo -u azureuser bash -c "cd $APP_DIR && git stash push -m 'auto-stash before update' && git pull origin $BRANCH && git stash pop || true"
+
 else
     sudo -u azureuser git clone -b $BRANCH "https://$GITHUB_TOKEN@$GIT_REPO" "$APP_DIR"
     sudo -u azureuser bash -c "cd $APP_DIR"
 fi
 
-# Install dependencies
-sudo -u azureuser /home/azureuser/my-python-app/venv/bin/pip install --upgrade pip
-sudo -u azureuser /home/azureuser/my-python-app/venv/bin/pip install -r "$APP_DIR/requirements.txt"
+# # Install dependencies
+# sudo -u azureuser /home/azureuser/SDA-bootcamp-project/venv/bin/pip install --upgrade pip
+# sudo -u azureuser /home/azureuser/SDA-bootcamp-project/venv/bin/pip install -r "$APP_DIR/requirements.txt"
 
 # Restart the service
 sudo systemctl restart myapp
