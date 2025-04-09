7b4fc7de6fd0bfb502d8d459530c85c3ef870d99
Ubuntu <azureuser@MyVMproject.qsielqogjuuejmis5pwymi0ref.bx.internal.cloudapp.net>
2025-04-09 08:28:14 +0000

    update .sh

--- update_app.sh
+++ update_app.sh
@@ -10,8 +10,8 @@ BRANCH="main"
 
 # Update code
 if [ -d "$APP_DIR" ]; then
-    # sudo -u azureuser bash -c "cd $APP_DIR && git pull origin $BRANCH"
-    sudo -u azureuser bash -c "cd $APP_DIR && git stash push -m 'auto-stash before update' && git pull origin $BRANCH && git stash pop || true"
+     sudo -u azureuser bash -c "cd $APP_DIR && git pull origin $BRANCH"
+    # sudo -u azureuser bash -c "cd $APP_DIR && git stash push -m 'auto-stash before update' && git pull origin $BRANCH && git stash pop || true"
 
 else
     sudo -u azureuser git clone -b $BRANCH "https://$GITHUB_TOKEN@$GIT_REPO" "$APP_DIR"
@@ -23,6 +23,6 @@ fi
 # sudo -u azureuser /home/azureuser/SDA-bootcamp-project/venv/bin/pip install -r "$APP_DIR/requirements.txt"
 
 # Restart the service
-sudo systemctl restart myapp
+sudo systemctl restart backend
 
 echo "Python application update completed!"
